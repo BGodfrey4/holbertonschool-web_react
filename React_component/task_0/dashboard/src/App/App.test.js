de213377e396cd5516241ae97f77ba4dd4fc9775
bgodfrey <4936@holbertonschool.com>
2023-06-23 10:44:21 +0000

    task_0 fix

--- React_component/task_0/dashboard/src/App/App.test.js
+++ React_component/task_0/dashboard/src/App/App.test.js
@@ -4,36 +4,36 @@ import React from 'react';
 
 
 describe('<App />', () => {
-  it('Will test that App will render without crashing', () => {
+  it('Tests that App renders without crashing', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.exists()).toBe(true);
   });
 
   // should contain the Notifications component
-  it('Will test that App will render a Notifications component', () => {
+  it('Tests that App renders a Notifications component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Notifications').exists()).toBe(true);
   });
 
   // should contain the Header component
-  it('Will test that App will render a Header component', () => {
+  it('Tests that App renders a Header component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Header').exists()).toBe(true);
   });
 
   // should contain the Login component
-  it('Will test that App will render a Login component', () => {
+  it('Tests that App renders a Login component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Login').exists()).toBe(true);
   });
 
   // should contain the Footer component
-  it('Will test that App will render a Footer component', () => {
+  it('Tests that App renders a Footer component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Footer').exists()).toBe(true);
   });
   // check that CourseList is not displayed when isLoggedIn is false
-  it('Will test that App does not render a CourseList component when isLoggedIn is false', () => {
+  it('Tests that App does not render a CourseList component when isLoggedIn is false', () => {
     const wrapper = shallow(<App isLoggedIn={false} />);
     expect(wrapper.find('CourseList').exists()).toBe(false);
   });
@@ -42,12 +42,12 @@ describe('<App />', () => {
 
 describe('<App /> with isLoggedIn', () => {
   // Login component is not displayed when isLoggedIn is true
-  it('Will test that App does not render a Login component when isLoggedIn is true', () => {
+  it('Tests that App does not render a Login component when isLoggedIn is true', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find('Login').exists()).toBe(false);
   });
   // CourseList component is displayed when isLoggedIn is true
-  it('Will test that App will render a CourseList component when isLoggedIn is true', () => {
+  it('Tests that App renders a CourseList component when isLoggedIn is true', () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find('CourseList').exists()).toBe(true);
   });
