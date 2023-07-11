09926884c8d820970b9e3dbcc45fae2d839baf50
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-11 05:09:24 -0500

    Additional Files

--- react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,66 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css, StyleSheet } from 'aphrodite';
-
-export default class CourseListRow extends React.Component {
-	constructor(props) {
-		super(props);
-		this.toggleChecked = this.toggleChecked.bind(this);
-		this.state = {
-			checked: false
-		}
-	}
-
-	toggleChecked = () => {
-		if (this.state.checked) this.setState({checked: false})
-		else this.setState({checked: true});
-	}
-
-	render () {
-	const styles = StyleSheet.create({
-		th: {
-			borderBottom: '1px solid lightgrey',
-			padding: '0.5rem',
-		},
-
-		rowChecked: {
-			backgroundColor: '#e6e4e4'
-		}
-	});
-
-	const headerStyle = '#deb5b545'
-	const regularStyle = '#f5f5f5ab'
-	const {isHeader, textFirstCell, textSecondCell} = this.props;
-
-		return (
-			<tr style={{ backgroundColor: isHeader ? headerStyle : regularStyle}}
-				className={this.state.checked ? css(styles.rowChecked) : null}>
-				{isHeader && textSecondCell === null && (
-					<th className={`th ${css(styles.th)}`}
-						colSpan={2}>{textFirstCell}</th>
-				)}
-				{isHeader && textSecondCell && (
-					<>
-						<th className={`th ${css(styles.th)}`}>{textFirstCell}</th>
-						<th className={`th ${css(styles.th)}`}>{textSecondCell}</th>
-					</>
-				)}
-				{!isHeader && (
-					<>
-						<td>
-							<label>
-								<input type='checkbox'
-									onChange={() => this.toggleChecked()} />
-								{textFirstCell}
-							</label>
-						</td>
-						<td>{textSecondCell}</td>
-					</>
-				)}
-			</tr>
-		)
-	}
-}
 
 CourseListRow.propTypes = {
 	isHeader: PropTypes.bool,
@@ -72,3 +11,30 @@ CourseListRow.defaultProps = {
 	isHeader: false,
 	textSecondCell: null,
 };
+
+const headerStyle = '#deb5b545'
+
+const regularStyle = '#f5f5f5ab'
+
+export default function CourseListRow ({ isHeader, textFirstCell,
+																				 textSecondCell}) {
+		return (
+			<tr style={{ backgroundColor: isHeader ? headerStyle : regularStyle}}>
+				{isHeader && textSecondCell === null && (
+					<th colSpan={2}>{textFirstCell}</th>
+				)}
+				{isHeader && textSecondCell && (
+					<>
+						<th>{textFirstCell}</th>
+						<th>{textSecondCell}</th>
+					</>
+				)}
+				{!isHeader && (
+					<>
+						<td>{textFirstCell}</td>
+						<td>{textSecondCell}</td>
+					</>
+				)}
+			</tr>
+	)
+}
