3045e65fa36c948e5ef1d318d053017c248f45b2
bgodfrey <4936@holbertonschool.com>
2023-07-13 04:15:33 +0000

    final tasks

--- react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
+++ react_redux_connectors_and_providers/task_8/dashboard/src/selectors/notificationSelector.js
@@ -1,13 +1,39 @@
-import { Map } from 'immutable';
+import { createSelector } from "reselect";
 
-export function filterTypeSelected(state) {
-  return state.get('filter');
-}
+export const filterTypeSelected = (state) => {
+  return state.get("filter");
+};
+export const getNotifications = (state) => {
+  return state.get("notifications");
+};
 
-export function getNotifications(state) {
-  return Map(state.get('notifications'));
-}
+const getNotificationsSelector = (state) => state.notifications;
 
-export function getUnreadNotifications(state) {
-  return Map(state.get('notifications')).filter((item => !item.isRead));
-}
+export const getUnreadNotificationsByType = createSelector(
+  getNotificationsSelector,
+  (notifications) => {
+    const messages = notifications.get("messages");
+    const filter = notifications.get("filter");
+
+    if (messages) {
+      let filtered;
+
+      if (filter === "URGENT") {
+        filtered = messages
+          .valueSeq()
+          .filter(
+            (value) =>
+              value.get("isRead") === false && value.get("type") === "urgent"
+          );
+      } else {
+        filtered = messages
+          .valueSeq()
+          .filter((value) => value.get("isRead") === false);
+      }
+
+      return filtered;
+    }
+
+    return messages;
+  }
+);
