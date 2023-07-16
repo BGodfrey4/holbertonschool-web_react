65c7152d9a226f7e74036a8e9ab280827fdf5e29
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-15 20:38:08 -0500

    rewritten App.test.js file

--- react_state/task_0/dashboard/src/App/App.test.js
+++ react_state/task_0/dashboard/src/App/App.test.js
@@ -1,10 +1,10 @@
+import sinon from 'sinon';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import { StyleSheetTestUtils } from 'aphrodite';
+import 'jsdom-global/register';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jsdom-global/register';
-import sinon from 'sinon';
-import { StyleSheetTestUtils } from 'aphrodite';
 
 window.alert = sinon.spy()
 
@@ -17,43 +17,43 @@ describe('<App />', () => {
 	afterEach(() => {
 		StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 	});
-	it('App renders w/o crashing', () => {
+	it('App will render succcessfully', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.exists()).toBe(true);
 	});
 
-	it('App renders Login when isLoggedIn=false', () => {
+	it('App performs Login if isLoggedIn=false', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('CourseList').length).toBe(0);
 	});
 
-	it('App renders CourseList when isLoggedIn=true', () => {
+	it('App successfully renders CourseList if isLoggedIn=true', () => {
 		const wrapper = shallow(<App isLoggedIn={true}/>);
 		expect(wrapper.find('Login').length).toBe(0);
 		expect(wrapper.find('CourseList').length).toBe(1);
 	});
 
-	it('Notifications renders', () => {
+	it('Notifications renders successfully', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('Notifications').exists()).toBe(true);
 	});
 
-	it('Header renders', () => {
+	it('Header successfully passes', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('Header').exists()).toBe(true);
 	});
 
-	it('Login renders', () => {
+	it('Login passes successfully', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('Login').exists()).toBe(true);
 	});
 
-	it('Footer renders', () => {
+	it('Footer passes successfully', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.find('Footer').exists()).toBe(true);
 	});
 
-	it('Default state of display Drawer is false & hide/display work', () => {
+	it(' Display Drawer is false & hide/display work', () => {
 		const wrapper = shallow(<App />);
 		expect(wrapper.state('displayDrawer')).toBe(false);
 		wrapper.instance().handleDisplayDrawer();
