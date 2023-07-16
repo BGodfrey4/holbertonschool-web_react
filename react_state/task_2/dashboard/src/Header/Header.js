8672e80ec2395bcd658fb2fb847c95ce1e1219d5
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-16 18:13:57 -0500

    Updated Header.js file

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import logo from '../assets/holberton-logo.jpg';
-import { css, StyleSheet } from 'aphrodite';
-import AppContext from '../App/AppContext';
 
-const styles = StyleSheet.create({
-  logo: {
-		width: '240px',
-		height: '240px'
-	},
+import { shallow } from "enzyme";
+import Login from "./Login";
+import React from "react";
+import { StyleSheetTestUtils } from "aphrodite";
 
-	header: {
-		display: 'block' 
-	},
-
-	header_info: {
-		display: 'flex',
-		alignItems: 'center'
-	}
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 });
 
-export default function Header() {
-	const { user, logOut } = React.useContext(AppContext);
-  return (
-		<React.Fragment>
-			<div className={css(styles.header)}>
-				<div className={css(styles.header_info)}>
-					<img src={logo} className={`App-logo ${css(styles.logo)}`} alt="logo" />
-					<h1>School Dashboard</h1>
-				</div>
-				{user.isLoggedIn ? (
-					<div>
-						<p id='logoutSection'>
-							Welcome {user.email}&nbsp;<a onClick={logOut} href='#'>(logout)</a>
-						</p>
-					</div> ) : (
-						null
-					)
-				}
-			</div>
-		</React.Fragment>
-  );
-}
+describe("Header", () => {
+  it("in thoery this functiuon renders without crashing", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("this function should have 3 input tags and 2 label tags", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find("label")).toHaveLength(2);
+    expect(wrapper.find("input")).toHaveLength(3);
+  });
+});
+
+describe("test for submit input on form", () => {
+  it("verify that the submit button is disabled by default", () => {
+    const wrapper = shallow(<Login />);
+
+    expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true);
+  });
+
+  it("checks to see after changing the value of two inputs, that button is enabled", () => {
+    const wrapper = shallow(<Login />);
+
+    wrapper.find("#email").simulate("change", { target: { value: "t" } });
+    wrapper.find("#password").simulate("change", { target: { value: "t" } });
+    expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true);
+  });
+});
