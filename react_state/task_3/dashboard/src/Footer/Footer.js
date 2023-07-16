99bfe53c1f02e2fd34c77b46274bb97a3a457d6f
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-16 18:31:12 -0500

    RE-doing the Footer file

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { getFooterCopy, getFullYear } from '../utils/utils';
 import AppContext from '../App/AppContext';
+import "./Footer.css";
 
-export default function Footer() {
-	const { user } = React.useContext(AppContext);
+function Footer() {
+  const { user } = useContext(AppContext);
 
   return (
-		<React.Fragment>
-			<p>Copyright {getFullYear()} - {getFooterCopy(0)}</p>
-			{user.isLoggedIn ? (
-				<p>
-					<a href='#'>Contact Us</a>
-				</p> ) : (
-					null
-				)
-			}
-		</React.Fragment>
+    <>
+      <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </>
   );
 }
+
+export default Footer;
