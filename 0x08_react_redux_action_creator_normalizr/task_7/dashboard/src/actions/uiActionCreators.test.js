2279cb6ff3b015a62c306e7019ea66ca0a2a91ea
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-17 00:36:23 -0500

    reduced contyent from file

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -1,143 +1,35 @@
-import {
-  Login, loginRequest, Logout,
-  displayNotificationDrawer, hideNotificationDrawer,
-  LoginSuccess, LoginFailure
-} from './uiActionCreators';
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
+import fetchMock from "fetch-mock";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import { login, logout, hideNotificationDrawer, displayNotificationDrawer } from "./uiActionCreators";
 
-import fetchMock from 'fetch-mock';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
+const mockStore = configureStore(middleWares);
+const middleWares = [thunk];
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+describe("testing UI notification action creators", () => {
+  it("should create proper action for login", () => {
+    const email = "james@gmail.com";
+    const password = "heheheh";
 
-describe('Login action creator', () => {
-  it('returns the correct action object', () => {
-    const email = 'test@example.com';
-    const password = 'password';
-    const expectedAction = {
-      type: 'LOGIN',
-      user: {
-        email: 'test@example.com',
-        password: 'password',
-      },
-    };
-
-    expect(Login(email, password)).toEqual(expectedAction);
-  });
-});
-
-describe('loginRequest', () => {
-  let dispatch;
-  beforeEach(() => {
-    dispatch = jest.fn();
-  });
-
-  it('dispatches LOGIN_SUCCESS when the json status is ok', async () => {
-    global.fetch = jest.fn().mockResolvedValue({
-      json: () => Promise.resolve({ status: 'ok' }),
-    });
-    await loginRequest()(dispatch);
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'LOGIN_SUCCESS',
+    expect(login(email, password)).toEqual({
+      type: LOGIN,
+      user: { email: "james@gmail.com", password: "heheheh" },
     });
   });
 
-  it('dispatches LOGIN_FAILURE when the json status is not ok', async () => {
-    global.fetch = jest.fn().mockResolvedValue({
-      json: () => Promise.resolve({ status: 'not ok' }),
+  it("produces proper action for logout", () => {
+    expect(logout()).toEqual({ type: LOGOUT });
+  });
+
+  it("produces proper action for displaying notification drawer", () => {
+    expect(displayNotificationDrawer()).toEqual({
+      type: DISPLAY_NOTIFICATION_DRAWER,
     });
-    await loginRequest()(dispatch);
-    expect(dispatch).toHaveBeenCalledWith({
-      type: 'LOGIN_FAILURE',
+  });
+
+  it("produces proper action for hiding notification drawer", () => {
+    expect(hideNotificationDrawer()).toEqual({
+      type: HIDE_NOTIFICATION_DRAWER,
     });
   });
-});
-
-describe('Logout action creator', () => {
-  it('returns the correct action object', () => {
-    const expectedAction = {
-      type: 'LOGOUT',
-    };
-
-    expect(Logout()).toEqual(expectedAction);
-  });
-});
-
-describe('displayNotificationDrawer action creator', () => {
-  it('returns the correct action object', () => {
-    const expectedAction = {
-      type: 'DISPLAY_NOTIFICATION_DRAWER',
-    };
-
-    expect(displayNotificationDrawer()).toEqual(expectedAction);
-  });
-});
-
-
-describe('hideNotificationDrawer action creator', () => {
-  it('returns the correct action object', () => {
-    const expectedAction = {
-      type: 'HIDE_NOTIFICATION_DRAWER',
-    };
-
-    expect(hideNotificationDrawer()).toEqual(expectedAction);
-  });
-});
-
-describe('LoginSuccess action creator', () => {
-  it('returns the correct action object', () => {
-    const expectedAction = {
-      type: 'LOGIN_SUCCESS',
-    };
-
-    expect(LoginSuccess()).toEqual(expectedAction);
-  });
-});
-
-describe('LoginFailure action creator', () => {
-  it('returns the correct action object', () => {
-    const expectedAction = {
-      type: 'LOGIN_FAILURE',
-    };
-
-    expect(LoginFailure()).toEqual(expectedAction);
-  });
-});
-
-describe('loginRequest', () => {
-  afterEach(() => {
-    fetchMock.restore();
-  });
-
-  it('dispatches LoginSuccess when login is successful', async () => {
-    fetchMock.getOnce('./login-success.json', { body: { status: 'ok' } });
-
-    const store = mockStore();
-    const email = 'test@example.com';
-    const password = 'password';
-    const expectedActions = [
-      Login(email, password),
-      LoginSuccess()
-    ];
-
-    await store.dispatch(loginRequest(email, password));
-    expect(store.getActions()).toEqual(expectedActions);
-  });
-
-it('dispatches LoginFailure when login fails', async () => {
-    fetchMock.getOnce('./login-success.json', { body: { status: 'error' } });
-
-    const store = mockStore();
-    const email = 'test@example.com';
-    const password = 'password';
-    const expectedActions = [
-      Login(email, password),
-      LoginFailure()
-    ];
-
-    await store.dispatch(loginRequest(email, password));
-    expect(store.getActions()).toEqual(expectedActions);
-  });
-
-});
