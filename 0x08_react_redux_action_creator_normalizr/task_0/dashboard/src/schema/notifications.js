24570ddf346ad32ab0a8f719528e339ff4950ad8
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-16 19:42:46 -0500

    edits for file

--- 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,15 +1,5 @@
-#!/usr/bin/node
-const notifications = require('../notifications.json');
+import * as notificationData from "../../../../notifications.json";
 
-
-
-function getAllNotificationsByUser(userId) {
-  // return a list containing all the context objects from the notifications.json data when the author id is the same as the userId
-  return notifications.filter((notification) => notification.author.id === userId)
-  .map((notification) => notification.context);
-
+export default function getAllNotificationsByUser(userId) {
+  return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
 }
-
-module.exports = getAllNotificationsByUser;
-
-// console.log(getAllNotificationsByUser('5debd764a7c57c7839d722e9'))
