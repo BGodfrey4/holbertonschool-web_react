88676d25599fbc10e1593901fbcdff1a03159c50
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-17 01:01:44 -0500

    Update notificationReducer.test.js

--- react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.test.js
+++ react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.test.js
@@ -1,70 +1,19 @@
-import notificationReducer, { initialState } from './notificationReducer';
-import { MARK_AS_READ, SET_TYPE_FILTER, NotificationTypeFilters, FETCH_NOTIFICATIONS_SUCCESS } from '../actions/notificationActionTypes';
+import { uiReducer, initialState } from './uiReducer';
+import * as actions from '../actions/uiActionTypes';
 
-const mockNotifications = [
-  {
-    id: 1,
-    type: 'default',
-    value: 'New course available',
-  },
-  {
-    id: 2,
-    type: 'urgent',
-    value: 'New resume available',
-  },
-  {
-    id: 3,
-    type: 'urgent',
-    value: 'New data available',
-  },
-];
-
-describe('notificationReducer', () => {
-  it('should return the initial state', () => {
-    expect(notificationReducer(undefined, {})).toEqual(initialState);
+describe('Testing uiReducer', () => {
+  it('verify the state returned by the uiReducer function equals the initial state when no action is passed', () => {
+    const res = uiReducer();
+    expect(res.toJS()).toStrictEqual(initialState.toJS());
   });
 
-  it('should handle MARK_AS_READ', () => {
-    const action = {
-      type: MARK_AS_READ,
-      index: 1,
-    };
-    const expectedState = {
-      filter: initialState.filter,
-      notifications: initialState.notifications.map((notification) => {
-        if (notification.id === action.index) {
-          return { ...notification, isRead: true };
-        }
-        return notification;
-      }),
-    };
-    expect(notificationReducer(initialState, action)).toEqual(expectedState);
+  it('verify  the state returned by the uiReducer function equals the initial state when the action SELECT_COURSE is passed', () => {
+    const res = uiReducer(undefined, {type: "SELECT_COURSE"});
+    expect(res.toJS()).toStrictEqual(initialState.toJS());
   });
 
-  it('should handle SET_TYPE_FILTER', () => {
-    const action = {
-      type: SET_TYPE_FILTER,
-      filter: NotificationTypeFilters.URGENT,
-    };
-    const expectedState = {
-      filter: action.filter,
-      notifications: initialState.notifications.map((notification) => {
-        return { ...notification, isRead: false };
-      }),
-    };
-    expect(notificationReducer(initialState, action)).toEqual(expectedState);
-  });
-  it('should handle FETCH_NOTIFICATIONS_SUCCESS', () => {
-    const action = {
-      type: FETCH_NOTIFICATIONS_SUCCESS,
-      data: mockNotifications,
-    };
-    const expectedState = {
-      filter: initialState.filter,
-      notifications: action.data.map((notification) => {
-        return { ...notification, isRead: false };
-      }),
-    };
-    expect(notificationReducer(initialState, action)).toEqual(expectedState);
+  it('verify the state returned by the uiReducer function, when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property', () => {
+    const res = uiReducer(undefined, {type: actions.DISPLAY_NOTIFICATION_DRAWER});
+    expect(res.toJS()).toStrictEqual({ ...initialState.toJS(), isNotificationDrawerVisible: true });
   });
 });
