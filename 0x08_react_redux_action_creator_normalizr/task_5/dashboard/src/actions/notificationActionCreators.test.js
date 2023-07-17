5528b096148297d8cf23b61b4e1621f12dea4823
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-16 23:57:15 -0500

    condensed and rearranged notificationActionCreators.test.js

--- 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.test.js
@@ -1,21 +1,16 @@
+import { NotificationTypeFilters, MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+
 import { markAsRead, setNotificationFilter } from "./notificationActionCreators";
 
-
-describe("notificationActionCreators", () => {
-  it("confirm markAsRead returns correct object", () => {
-    const index = 1;
-    const expectedAction = {
-      type: "MARK_AS_READ",
-      index,
-    };
-    expect(markAsRead(index)).toEqual(expectedAction);
+describe("tests for notification actions", () => {
+  it("should create right action for mark as read", () => {
+    expect(markAsRead(1)).toEqual({ type: MARK_AS_READ, index: 1 });
   });
-  it("confirm setNotificationFilter returns correct object", () => {
-    const filter = "DEFAULT";
-    const expectedAction = {
-      type: "SET_TYPE_FILTER",
-      filter,
-    };
-    expect(setNotificationFilter(filter)).toEqual(expectedAction);
+
+  it("should create right action for notification filter", () => {
+    expect(setNotificationFilter(NotificationTypeFilters["DEFAULT"])).toEqual({
+      type: SET_TYPE_FILTER,
+      filter: "DEFAULT",
+    });
   });
 });
