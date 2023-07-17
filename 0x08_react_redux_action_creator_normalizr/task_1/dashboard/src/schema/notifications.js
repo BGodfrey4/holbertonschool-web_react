0ab9351819b824c17a65b9ec936989fee1453821
BGodfrey4 <104100138+BGodfrey4@users.noreply.github.com>
2023-07-16 19:51:27 -0500

    Update notifications.js

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,30 +1,17 @@
-#!/usr/bin/node
-// const notifications = require('../notifications.json');
-import notifications from '../notifications.json';
-import { schema, normalize } from 'normalizr';
+import * as notificationData from "../../../../notifications.json";
+import { normalize, schema } from "normalizr";
 
+const user = new schema.Entity("users");
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+const notification = new schema.Entity("notification", {
+  author: user,
+  context: message,
+});
 
+const normalized = normalize(notificationData, [notification]);
 
 export default function getAllNotificationsByUser(userId) {
-  // return a list containing all the context objects from the notifications.json data when the author id is the same as the userId
-  return notifications.filter((notification) => notification.author.id === userId)
-  .map((notification) => notification.context);
-
+  return notificationData.filter((notification) => notification.author.id === userId).map((notification) => notification.context);
 }
 
-const user = new schema.Entity('users');
-
-const messages = new schema.Entity('messages', {}, {
-  idAttribute: 'guid',
-});
-
-const notificationSchema = new schema.Entity('notifications', {
-  author: user,
-  context: messages,
-});
-
-export const normalizedData = normalize(notifications, [notificationSchema]);
-
-// console.log(normalizedData.entities.users)
-
-// console.log(getAllNotificationsByUser('5debd764a7c57c7839d722e9'))
+export { normalized };
